d8d4756660ac17441273b02207f16b1429d66633
Tyler Tollin <43883994+TylerTollin@users.noreply.github.com>
2025-08-24 11:28:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ session = cnx.session()
 
 my_dataframe = (
     session.table("smoothies.public.fruit_options")
-        .select(col("FRUIT_NAME"))
+        # .select(col("FRUIT_NAME"))
 )
 
 st.dataframe(data=my_dataframe, use_container_width=True)
